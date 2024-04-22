06031b1a7396431b89698cbabe5d77f65f5fa70b
Álan Ribeiro Sparremberger <alan.sparremberger@ntconsult.com.br>
2024-04-22 07:49:50 -0300

    jeijei

--- jb-front/src/components/Home.js
+++ jb-front/src/components/Home.js
@@ -19,33 +19,11 @@ import EndlessText from "./EndlessText.js";
 import InstaCarousel from "./InstaCarousel.js";
 import MultiCarousel from "./MultiCarousel.js";
 
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "../@/components/ui/dialog.tsx";
-
-
 const Home = () => {
     return (
         <div>
             <Carrousel />
-            <Dialog>
-                <DialogTrigger>Open</DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Are you absolutely sure?</DialogTitle>
-                        <DialogDescription>
-                            This action cannot be undone. This will permanently
-                            delete your account and remove your data from our
-                            servers.
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
+           
 
             <MultiCarousel />
             <WelcomeBar />
